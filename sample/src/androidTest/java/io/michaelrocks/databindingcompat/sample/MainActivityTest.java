cfa6cbcda66c2d494fcfbf8f1eac23d222b4ae70
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2017-05-12 20:19:18 +0300

    Add an Android sample for testing the plugin

---
+++ sample/src/androidTest/java/io/michaelrocks/databindingcompat/sample/MainActivityTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.michaelrocks.databindingcompat.sample;
+
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.os.Build;
+import android.support.graphics.drawable.VectorDrawableCompat;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.ImageView;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+  @Rule
+  public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+  @Test
+  public void checkImageViewContainsVectorDrawable() {
+    onView(withId(R.id.image))
+        .check(matches(withVectorDrawable()));
+  }
+
+  private static Matcher<View> withVectorDrawable() {
+    return new BoundedMatcher<View, ImageView>(ImageView.class) {
+      public void describeTo(Description description) {
+        description.appendText("with vector drawable");
+      }
+
+      public boolean matchesSafely(ImageView imageView) {
+        final Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof VectorDrawableCompat) {
+          return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+          return drawable instanceof VectorDrawable;
+        }
+
+        return false;
+      }
+    };
+  }
+}
